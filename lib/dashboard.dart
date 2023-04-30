55d4d5784e9d168d00da3c05959bdf7e35fd116d
Bazz <abbasirekeola@gmail.com>
2023-04-30 15:27:43 -0500

    card update

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -11,6 +12,69 @@ class Dashboard extends StatefulWidget {
 }
 
 class _DashboardState extends State<Dashboard> {
+  int _current = 0;
+  final CarouselController _controller = CarouselController();
+
+  final List<Widget> cardsList = [
+    Card(
+      margin: EdgeInsets.zero,
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(
+                left: 8.0, right: 15, top: 10, bottom: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  margin: EdgeInsets.all(12.0),
+                  padding: EdgeInsets.all(8.0),
+                  decoration: BoxDecoration(
+                    color: Color(0xFFC883FF),
+                    borderRadius: BorderRadius.all(Radius.circular(4)),
+                  ),
+                  child: Text(
+                    'High Priority',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Icon(Icons.more_horiz_outlined)
+              ],
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 10,vertical: 15),
+            child: Column(
+              children: [
+                Text(
+                  'Lorem ipsum dolor sit amet consectetur.',
+                  style: TextStyle(
+                    fontSize: 22,
+                  ),
+                ),
+                SizedBox(height: 10,),
+                Text(
+                  'Lorem ipsum dolor sit amet consectetur. Egestas justo nisl lorem urna.',
+                  style: TextStyle(
+                    fontSize: 15,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    ),
+    Card(
+      child: Text('Card 2'),
+    ),
+    Card(
+      child: Text('Card 3'),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,7 +103,7 @@ class _DashboardState extends State<Dashboard> {
             ],
           ),
           Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0,vertical: 10),
+            padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               mainAxisAlignment: MainAxisAlignment.start,
@@ -72,8 +136,8 @@ class _DashboardState extends State<Dashboard> {
             child: DefaultTabController(
               length: 2,
               child: Column(
-                children: const [
-                  TabBar(
+                children: [
+                  const TabBar(
                     tabs: [
                       Tab(
                         child: Text(
@@ -99,7 +163,64 @@ class _DashboardState extends State<Dashboard> {
                   Expanded(
                     // wrap the TabBarView widget in an Expanded widget
                     child: TabBarView(children: [
-                      Text('data 1'),
+                      Column(
+                        children: [
+                          SizedBox(height: 20,),
+                          CarouselSlider(
+                            carouselController: _controller,
+                            options: CarouselOptions(
+                              onPageChanged: (index, reason) {
+                                setState(() {
+                                  _current = index;
+                                });
+                              },
+                              height: 230.0,
+                              enlargeCenterPage: false,
+                              autoPlay: false,
+                              aspectRatio: 16 / 9,
+                              autoPlayCurve: Curves.fastOutSlowIn,
+                              enableInfiniteScroll: false,
+                              autoPlayAnimationDuration:
+                                  Duration(milliseconds: 800),
+                              viewportFraction: 0.90,
+                            ),
+                            items: cardsList.map((card) {
+                              return Builder(
+                                builder: (BuildContext context) {
+                                  return Container(
+                                    width: getScreen(context).width,
+                                    child: card,
+                                  );
+                                },
+                              );
+                            }).toList(),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: cardsList.asMap().entries.map((entry) {
+                              return GestureDetector(
+                                onTap: () =>
+                                    _controller.animateToPage(entry.key),
+                                child: Container(
+                                  width: 12.0,
+                                  height: 12.0,
+                                  margin: EdgeInsets.symmetric(
+                                      vertical: 8.0, horizontal: 4.0),
+                                  decoration: BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      color: (Theme.of(context).brightness ==
+                                                  Brightness.dark
+                                              ? Colors.white
+                                              : Colors.black)
+                                          .withOpacity(_current == entry.key
+                                              ? 0.9
+                                              : 0.4)),
+                                ),
+                              );
+                            }).toList(),
+                          ),
+                        ],
+                      ),
                       Text('data 2'),
                     ]),
                   )
